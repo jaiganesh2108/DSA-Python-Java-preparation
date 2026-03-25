d11fa94494c276c8e78eab0523eebea1fd5f77bd
jaiganesh2108 <jaig7335@gmail.com>
2026-03-25 22:02:09 +0530

    re module

--- one.py
+++ one.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 print(json.dumps({"name" : "jai", "age": 22})) #dictionary
 print(json.dumps(["apple", "banana"])) #list
@@ -9,3 +10,31 @@ print(json.dumps(50.2)) #float
 print(json.dumps(True)) #boolean
 print(json.dumps(False)) #boolean
 print(json.dumps(None)) #Null
+
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": False,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# use . and a space to separate objects, and a space, a = and a space to separate keys from their values:
+print(json.dumps(x, indent=4, separators=(". ", " = ")))
+print("-----------------------------")
+
+#Check if the string starts with "The" and ends with "Spain":
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+
+if x:
+  print("YES! We have a match!")
+else:
+  print("No match")
