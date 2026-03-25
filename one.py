9179100d99d13e66e9d3a11afc9653a440c2a82e
jaiganesh2108 <jaig7335@gmail.com>
2026-03-25 21:49:37 +0530

    additional stuffs

--- one.py
+++ one.py
@@ -1,3 +1,11 @@
 import json
 
-print(json.dumps({"name" : "jai", "age": 22}))
+print(json.dumps({"name" : "jai", "age": 22})) #dictionary
+print(json.dumps(["apple", "banana"])) #list
+print(json.dumps(("apple", "banana"))) #tuple
+print(json.dumps("hello")) #string
+print(json.dumps(42)) #int
+print(json.dumps(50.2)) #float
+print(json.dumps(True)) #boolean
+print(json.dumps(False)) #boolean
+print(json.dumps(None)) #Null
