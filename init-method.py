79e02889127f7838bb0f4f8878c1ef4827e5c97b
jaiganesh2108 <jaig7335@gmail.com>
2026-03-26 20:10:01 +0530

    problem statement

--- init-method.py
+++ init-method.py
@@ -1,3 +1,11 @@
+"""Instructions
+Inside the editor, complete the following steps:
+Create a class called Dog
+Add an __init__ method with parameters name and age, and store them as properties using self
+Add a method called bark that prints the dog's name followed by " says Woof!"
+Create an object d1 of the Dog class with name "Buddy" and age 3
+Call the bark method on d1"""
+
 class Dog:
     def __init__(self, name, age):
         self.name = name
