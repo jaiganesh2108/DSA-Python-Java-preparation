d6787d5b484ca56186555aba8340bd2b08e50ed5
jaiganesh2108 <jaig7335@gmail.com>
2026-03-26 19:49:16 +0530

    __init__() method

--- oops-day1.py
+++ oops-day1.py
@@ -1,8 +1,9 @@
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age=18): #default value for age is 18 if not provided
         self.name = name
         self.age = age
 
-p1 = Person("John", 36)
-
+p1 = Person("John") #since age is not provided, it will take the default value of 18
+p2 = Person("jane", 24) #since age is provided, it will take the value of 24
 print(p1.name, p1.age)
+print(p2.name, p2.age)
