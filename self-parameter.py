cc6f5794c2168d35022971c3954a59855e4be965
jaiganesh2108 <jaig7335@gmail.com>
2026-03-27 19:57:04 +0530

    self parameter practice

---
+++ self-parameter.py
@@ -0,0 +1,9 @@
+class Example():
+    a = 10
+    def add(self, a):
+        self.a = a #this will create an instance variable a that shadows the class variable a
+
+exc = Example()
+print(exc.a)
+exc.add(20)
+print(exc.a)
