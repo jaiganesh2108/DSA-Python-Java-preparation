8c88caae39229f557a30e9b5c7437e9859862000
jaiganesh2108 <jaig7335@gmail.com>
2026-04-02 09:46:38 +0530

    feat: variable commit

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("java is fun!");
+        System.out.println("thsi is the first print");
+        String name = "Jai";
+        System.out.println(name);
     }
 }
