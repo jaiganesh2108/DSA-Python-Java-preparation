6e9d2c1ea2dc4e25cb5a50e919833eee064f193a
jaiganesh2108 <jaig7335@gmail.com>
2026-05-10 11:40:07 +0530

    selection sort commit 2

--- selection_sort.py
+++ selection_sort.py
@@ -7,7 +7,6 @@ for i in range(n-1):
     for j in range(i+1, n):
         if my_array[j] < my_array[min_index]:
             min_index = j
-        min_value = my_array.pop(min_index)
-        my_array.insert(i, min_value)
+    my_array[i], my_array[min_index] = my_array[min_index], my_array[i]
 
 print("sorted array is:", my_array) 
