d2bda891cfad849ce94d20dba6186895c38e390a
jaiganesh2108 <jaig7335@gmail.com>
2026-05-11 11:09:13 +0530

    CRUD test one

---
+++ crud.py
@@ -0,0 +1,30 @@
+student = {}
+
+# Create
+def create_student(name, id):
+    student[id] = name
+    print(f"student {name} with id {id} created")
+
+# Read
+def read_student():
+    print("\nStudent records")
+    for id, name in student.items():
+        print(f"id : {id}, name : {name}")
+
+# Update
+def update_student(id, new_name):
+    if id in student:
+        student[id] = new_name
+        print(f"student with id {id} updated to {new_name}")
+    else:
+        print("student not found")
+
+# Delete
+def delete_student(id):
+    if id in student:
+        del student[id]
+        print(f"student with id {id} deleted")
+    else:
+        print("student not found")
+
+# testing CRUD Operations
