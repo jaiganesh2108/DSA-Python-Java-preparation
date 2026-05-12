6a59b08ab966991284922f91a6978d116c42d5e1
jaiganesh2108 <jaig7335@gmail.com>
2026-05-12 20:52:58 +0530

    feat: common string prefix

---
+++ common_string.py
@@ -0,0 +1,16 @@
+strs = ["flower","flow","flight"]
+
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+    
+    prefix = strs[0]
+    for s in strs[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+
+            if prefix == "":
+                return ""
+    return prefix
+
+print(longestCommonPrefix(strs))
