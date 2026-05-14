70ce77110e9faa7e2e07aaf97924cc1654fbdb79
jaiganesh2108 <jaig7335@gmail.com>
2026-05-14 20:08:11 +0530

    feat: counting_sort

---
+++ counting_sort.py
@@ -0,0 +1,19 @@
+def counting_sort(arr):
+    if not arr:
+        return arr
+    
+    max_val = max(arr)
+    count = [0] * (max_val + 1)
+
+    for num in arr:
+        count[num] += 1
+    arr[:] = []
+
+    for num, freq in enumerate(count):
+        arr.extend([num] * freq)
+    return arr
+
+unsorted_arr = [4, 2, 2, 8, 3, 3, 1]
+sorted_arr = counting_sort(unsorted_arr)
+print("Sorted array:", sorted_arr)
+    
